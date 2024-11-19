218179cad4773499394e77c8f655472ec08a46bb
Ryan Hoffmann <ryan@hoff.pub>
2024-11-18 19:10:16 -0600

    remove example plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,13 +1,11 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
-import { IncrementCounter } from "./actions/increment-counter";
 import { ToggleDevice } from "./actions/toggle-device";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.TRACE);
 
-// Register the increment action.
-streamDeck.actions.registerAction(new IncrementCounter());
+// Register the actions that this plugin supports.
 streamDeck.actions.registerAction(new ToggleDevice());
 
 // Finally, connect to the Stream Deck.
